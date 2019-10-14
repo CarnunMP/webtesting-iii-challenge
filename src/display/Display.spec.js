a097284a62b13ca4c6ddd392e43f22de19711a38
CarnunMP <carnun@hotmail.co.uk>
2019-10-14 14:17:50 +0100

    testing boilerplate added; stable build reached

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,18 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./Display";
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+beforeEach(() => {
+
+    wrapper = rtl.render(<Display locked={false} closed={false} />);
+});
+
+describe("Display component", () => {
+    it("can debug the output", () => {
+        wrapper.debug;
+    });
+});
